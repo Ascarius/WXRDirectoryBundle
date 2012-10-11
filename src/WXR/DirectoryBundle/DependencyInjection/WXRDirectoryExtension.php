13a3b95c9acefa85a4843d19534da04bd00f05ad
Lionel Gaillard <lionel@wxrstudios.com>
2012-10-11 16:35:36 +0200

    Fixed WXRDirectoryExtension

--- src/WXR/DirectoryBundle/DependencyInjection/WXRDirectoryExtension.php
+++ src/WXR/DirectoryBundle/DependencyInjection/WXRDirectoryExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Westudio\ORLBundle\DependencyInjection;
+namespace WXR\DirectoryBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
