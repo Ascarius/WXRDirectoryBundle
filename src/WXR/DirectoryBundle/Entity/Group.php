eb8891976a5079b157445d434e8da9171fda9593
Lionel Gaillard <lionel@wxrstudios.com>
2012-11-01 15:24:03 +0100

    Group extends BaseCategory

--- src/WXR/DirectoryBundle/Entity/Group.php
+++ src/WXR/DirectoryBundle/Entity/Group.php
@@ -4,11 +4,11 @@ namespace WXR\DirectoryBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 
-use WXR\CategoryBundle\Entity\Category;
+use WXR\CategoryBundle\Entity\BaseCategory;
 use WXR\DirectoryBundle\Model\ContactInterface;
 use WXR\DirectoryBundle\Model\GroupInterface;
 
-class Group extends Category implements GroupInterface
+class Group extends BaseCategory implements GroupInterface
 {
     /**
      * @var ArrayCollection
