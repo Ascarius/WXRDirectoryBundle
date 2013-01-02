fa050e487b36bfe656df9fe83db64d7480ec53c8
Lionel Gaillard <lionel@wxrstudios.com>
2013-01-02 15:57:36 +0100

    Adds searchable properties

--- Entity/ContactManager.php
+++ Entity/ContactManager.php
@@ -20,6 +20,43 @@ class ContactManager extends BaseManager implements ContactManagerInterface
     /**
      * {@inheritDoc}
      */
+    public function getSearchableProperties()
+    {
+        return array(
+            $this->alias.'.fullname',
+            'group.name'
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function buildFromClause(QueryBuilder $qb, array $criteria)
+    {
+        parent::buildFromClause($qb, $criteria);
+
+        $needJoins = false;
+
+        foreach (array('_search', 'group') as $needle) {
+            foreach ($criteria as $criterium => $value) {
+                if (false !== strpos($criterium, $needle)) {
+                    $needJoins = true;
+                    break 2;
+                }
+            }
+        }
+
+        if ($needJoins) {
+            die('need');
+            $qb
+                ->leftJoin($this->alias.'.groups', 'group')
+            ;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     protected function buildWhereClause(QueryBuilder $qb, array $criteria)
     {
         if (!array_key_exists('enabled', $criteria)) {
